fe2194be66bd4b9a41b9003c0a32b21186c637f6
anannyachy54 <105448900+anannyachy54@users.noreply.github.com>
2023-12-20 23:23:06 +0600

    Basic JS_C191209

---
+++ index.js
@@ -0,0 +1,37 @@
+// x = 5;
+// console.log(x);
+
+//   var nName="This is a text";
+//   console.log(nName);
+
+// var n1 = 10;
+// var  n2 = 2;
+// // var res1= n1+n2;
+// // var res2= n2%n1;
+// var res3= n1**n2;
+// console.log(res3);
+ 
+// var n=5;
+// var m=3;
+// n++;
+// m--;
+// console.log(n);
+// console.log(m);
+
+// var n=4;
+// // // n+=10;
+// // n*=2;
+// // n**=2;
+// console.log(n);
+
+// var n1="This is a string";
+// console.log(typeof(n1));
+
+
+
+
+var w=prompt("Enter width");
+var hei=prompt("Enter height");
+var area=w*hei;
+document.write("The area of rectangle: "+ area +'<br>');
+document.write(typeof(area));
